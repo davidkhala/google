4d18bb3b1a657f608d521b519be075cb118d2546
David Liu <david-khala@hotmail.com>
2025-05-26 13:40:34 +0800

    backup

--- maps/node/tests/places.js
+++ maps/node/tests/places.js
@@ -26,6 +26,52 @@ describe('try one', function () {
         console.debug(data)
 
     })
+    it('Tokyo, Nihonbashi 3-Chome', async ()=>{
+        const data = await client.search("HSBC Building 11-1, Nihonbashi 3-chome,Chuo-ku")
+        assert.equal(data.length, 1)
+        console.debug(data)
+    })
+    it('Republic of Korea', async ()=>{
+        const data = await client.search("서울 칠패로 37, HSBC Building")
+        assert.equal(data.length, 1)
+        console.debug(data)
+    })
+    it('New Zealand', async ()=>{
+        let data = await client.search("Wellington 6011")
+        assert.equal(data.length, 1)
+        console.debug(data)
+        data = await client.search("188 Quay Street Auckland 1010")
+        assert.equal(data.length, 1)
+        console.debug(data)
+    })
+
+    it('Singapore', async ()=>{
+        // SingLand Tower
+        let data = await client.search('HSBC (Raffles Place Branch)')
+        assert.equal(data.length, 1)
+        console.debug(data)
+        data = await client.search('9 Battery Road #12-01 MYP Centre')
+        assert.equal(data.length, 1)
+        console.debug(data)
+    })
+
+    it('vietnam', async ()=>{
+        let data = await client.search('Flemington Tower, 182 Đ. Lê Đại Hành, Phường 15, Quận 11, Hồ Chí Minh, 越南')
+        assert.equal(data.length, 1)
+        console.debug(data)
+        // IDMC Building
+        data = await client.search('15 Phạm Hùng, Phường Mỹ Đình 2, Quận Nam Từ Liêm, Thành phố Hà Nội')
+        assert.equal(data.length, 1)
+        console.debug(data)
+        // Hanoi, Golden Palace Le Van Luong
+        data = await client.search('2R32+PX4, Trung Hòa Nhân Chính, Thanh Xuân, Hà Nội, 越南')
+        assert.equal(data.length, 1)
+        console.debug(data)
+        // Viet Tower
+        data = await client.search('1 P. Thái Hà, Trung Liệt, Đống Đa, Hà Nội, 越南')
+        assert.equal(data.length, 1)
+        console.debug(data)
+    })
 })
 
 function load_csv(name) {
@@ -36,7 +82,7 @@ function load_csv(name) {
 
 describe('batch', function () {
     this.timeout(0)
-    const task = async (name) => {
+    const search = async (name) => {
         const [searchTexts, raw] = load_csv(name)
         for (let i = 0; i < raw.length; i++) {
             const item = searchTexts[i]
@@ -56,24 +102,27 @@ describe('batch', function () {
         fs.writeFileSync(`tests/out/${name}2.csv`, ToFile(raw))
     }
     it('AUS', async () => {
-        await task('aus')
+        await search('aus')
     })
     it('malaysia', async () => {
-        await task('malaysia')
+        await search('malaysia')
     })
     it('Bangladesh', async () => {
-        await task('bangladesh')
+        await search('bangladesh')
     })
     it('vietnam', async () => {
-        await task('vietnam')
+        await search('vietnam')
     })
     it('Indonesia', async () => {
-        await task('indon')
+        await search('indon')
     })
     it('Sri Lanka', async () => {
-        await task('sri-lanka')
+        await search('sri-lanka')
     })
     it('Philippines', async () => {
-        await task('philip')
+        await search('philip')
+    })
+    it('Singapore', async () => {
+        await search('sin')
     })
 })
