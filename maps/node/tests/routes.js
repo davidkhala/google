45379ca0a483d23d63e8aa30821b8636379f3308
David Liu <david-khala@hotmail.com>
2025-06-04 13:58:57 +0800

    done routes

--- maps/node/tests/routes.js
+++ maps/node/tests/routes.js
@@ -47,7 +47,7 @@ describe('Malaysia', function () {
         assert.equal((await client.route(Sandakan, Tawau, RouteTravelMode.DRIVE))[0].durationHours, '4.9')
     })
 })
-describe('Indonesia', function (){
+describe('Indonesia', function () {
     this.timeout(0)
     it('Jawa Tengah', async () => {
         const Semarang = {
@@ -58,5 +58,77 @@ describe('Indonesia', function (){
         }
         const [route] = await client.route(Semarang, Surakarta)
         assert.equal(route.durationHours, '2.6')
+    })
+    it('Kalimantan', async () => {
+        const Samarinda = {
+            latitude: -0.5014884, longitude: 117.1451361,
+        }
+        const Balikpapan = {
+            latitude: -1.27684, longitude: 116.8377279,
+        }
+        const [route] = await client.route(Samarinda, Balikpapan, RouteTravelMode.DRIVE)
+        assert.equal(route.durationHours, '2.0')
+    })
+    it('java island', async () => {
+        const Malang = {
+            latitude: -7.9862201, longitude: 112.6341398,
+        }
+        // one point among Surabaya
+        const Surabaya = {
+            latitude: -7.2912248, longitude: 112.716962,
+        }
+        const [route] = await client.route(Malang, Surabaya)
+        assert.equal(route.durationHours, '3.0')
+    })
+    it('Sumatra Island', async () => {
+        const Pekanbaru = {
+            latitude: 0.5353713, longitude: 101.4276199,
+        }
+        // one point among medan
+        const Medan = {
+            latitude: 3.5977889, longitude: 98.679518
+        }
+        const [route] = await client.route(Pekanbaru, Medan, RouteTravelMode.DRIVE)
+        assert.equal(route.durationHours, '12.2')
+    })
+    it('Singapore Strait', async () => {
+        const Batam = {
+            latitude: 1.1393561, longitude: 104.0155464,
+        }
+        // one point among Singapore
+        const Singapore = {
+            latitude: 1.2746046, longitude: 103.7990299
+        }
+        const [route] = await client.route(Singapore, Batam, RouteTravelMode.DRIVE)
+        assert.equal(route.durationHours, '5.7')
+    })
+    it('Sri Lanka', async ()=>{
+        const Negombo = {
+            latitude: 7.2095698, 	longitude:79.8491076
+        }
+        // one point among Colombo
+        const Colombo= {
+            latitude: 6.8962205, 	longitude: 79.8568925
+        }
+        assert.equal((await client.route(Negombo, Colombo))[0].durationHours, '1.5')
+
+        const Moratuwa = {
+            latitude:6.7802707, 	longitude:79.8835025
+        }
+        assert.equal((await client.route(Moratuwa, Colombo))[0].durationHours, '0.9')
+        const Pelawatte = {
+            latitude:6.8902203, 	longitude: 79.9289717
+        }
+        assert.equal((await client.route(Pelawatte, Colombo))[0].durationHours, '0.7')
+    })
+    it('Philippines', async ()=>{
+        // 2 points around Manila
+        const MuntinlupaCity = {
+            latitude: 14.4248718, 	longitude: 121.0274923
+        }
+        const Makati = {
+            latitude: 14.5535847, 	longitude: 121.0261037
+        }
+        assert.equal((await client.route(MuntinlupaCity, Makati))[0].durationHours, '1.8')
     })
 })
